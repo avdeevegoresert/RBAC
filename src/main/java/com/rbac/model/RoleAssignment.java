b15832fb495411307320d0ec491f497fd8b3499d
Авдеев Егор Игоревич <ip313s01@csc.sibsutis.ru>
2026-04-27 10:37:53 +0700

    add RoleAssignment interface 1.5

---
+++ src/main/java/com/rbac/model/RoleAssignment.java
@@ -0,0 +1,10 @@
+package com.rbac.model;
+
+public interface RoleAssignment {
+    String assignmentId();
+    User user();
+    Role role();
+    AssignmentMetadata metadata();
+    boolean isActive();
+    String assignmentType();
+}
